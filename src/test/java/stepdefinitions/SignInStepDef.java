2fc6adafd554995528a7ced18327455ae96853c1
iskendernuori <isknuori@gmail.com>
2020-11-11 10:08:25 +0200

    Page package e SiginPage eklendi configuration.properties e api eklendi

---
+++ src/test/java/stepdefinitions/SignInStepDef.java
@@ -0,0 +1,13 @@
+package stepdefinitions;
+import com.sun.tools.jxc.ConfigReader;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+
+public class SignInStepDef {
+
+
+
+}

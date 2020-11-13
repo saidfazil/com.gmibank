fef995c29e6e5196ea8eb3c1454c830ddc5a8168
mustafaoz006 <moz006045@gmail.com>
2020-11-14 00:29:23 +0100

    US_013 is finished 13/11/2020.

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@US02_CharecterTest",
+        tags = "@US12",
         dryRun = false
 )
 

bb26f53f95150ff930370754a538c0dc6a4c5b1d
ishaksengul <sengulishak@gmail.com>
2020-11-22 14:10:32 +0100

    local master updated

--- target/default-cucumber-reports/report.js
+++ target/default-cucumber-reports/report.js
@@ -1,43 +1,111 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/US27ApiDeleteStates.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/US03passwordCreate.feature");
 formatter.feature({
-  "name": "API Test",
+  "name": "Registration page should restrict password usage to a secure and high level passcode",
   "description": "",
   "keyword": "Feature",
   "tags": [
     {
-      "name": "@US27"
+      "name": "@US03"
     }
   ]
 });
-formatter.scenario({
-  "name": "System should allow to delete states using api end point",
+formatter.background({
+  "name": "User go to the GMi Bank home page (https://gmibank.com/)",
   "description": "",
-  "keyword": "Scenario",
-  "tags": [
-    {
-      "name": "@US27"
-    }
-  ]
+  "keyword": "Background"
 });
 formatter.step({
-  "name": "The user connects to the API with the token given to her",
+  "name": "User go to the GMi Bank home page",
   "keyword": "Given "
 });
 formatter.match({
-  "location": "stepdefinitions.US27ApiStepdefs.the_user_connects_to_the_API_with_the_token_given_to_her()"
+  "location": "stepdefinitions.PasswordCreateStepDef.userGoToTheGMiBankHomePage()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "The user should be delete a state",
+  "name": "Click on user button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefinitions.PasswordCreateStepDef.clickOnUserButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "You should navigate to registration page",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefinitions.PasswordCreateStepDef.youShouldNavigateToRegistrationPage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on Password textbox",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefinitions.PasswordCreateStepDef.clickOnPasswordTextbox()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "There should be at least 1 lowercase char for stronger password and see the level chart change accordingly",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@US03"
+    },
+    {
+      "name": "@US03TC01"
+    }
+  ]
+});
+formatter.step({
+  "name": "Enter a password with 1 lowercase letter of at least 7 characters",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefinitions.PasswordCreateStepDef.enterAPasswordWithLowercaseLetterOfAtLeastCharacters()"
+});
+formatter.result({
+  "error_message": "java.lang.NullPointerException\n\tat stepdefinitions.PasswordCreateStepDef.enterAPasswordWithLowercaseLetterOfAtLeastCharacters(PasswordCreateStepDef.java:40)\n\tat ✽.Enter a password with 1 lowercase letter of at least 7 characters(file:///Users/administrator/IdeaProjects/com.gmibank/src/test/resources/features/US03passwordCreate.feature:13)\n",
+  "status": "failed"
+});
+formatter.step({
+  "name": "Color line must be yellow or green",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefinitions.PasswordCreateStepDef.colorLineMustBeYelloweOrGreen()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Click on password confirmation textbox",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefinitions.PasswordCreateStepDef.clickOnPasswordConfirmationTextbox()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Enter to second textbox with 1 lowercase letter of at least 7 characters",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "stepdefinitions.US27ApiStepdefs.theUserShouldBeDeleteAState()"
+  "location": "stepdefinitions.PasswordCreateStepDef.enterToSecondTextboxWithLowercaseLetterOfAtLeastCharacters()"
 });
 formatter.result({
-  "error_message": "java.lang.AssertionError: 1 expectation failed.\nExpected status code \u003c404\u003e but was \u003c204\u003e.\n\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.codehaus.groovy.reflection.CachedConstructor.invoke(CachedConstructor.java:72)\r\n\tat org.codehaus.groovy.reflection.CachedConstructor.doConstructorInvoke(CachedConstructor.java:59)\r\n\tat org.codehaus.groovy.runtime.callsite.ConstructorSite$ConstructorSiteNoUnwrap.callConstructor(ConstructorSite.java:84)\r\n\tat org.codehaus.groovy.runtime.callsite.CallSiteArray.defaultCallConstructor(CallSiteArray.java:59)\r\n\tat org.codehaus.groovy.runtime.callsite.AbstractCallSite.callConstructor(AbstractCallSite.java:263)\r\n\tat org.codehaus.groovy.runtime.callsite.AbstractCallSite.callConstructor(AbstractCallSite.java:277)\r\n\tat io.restassured.internal.ResponseSpecificationImpl$HamcrestAssertionClosure.validate(ResponseSpecificationImpl.groovy:493)\r\n\tat io.restassured.internal.ResponseSpecificationImpl$HamcrestAssertionClosure$validate$1.call(Unknown Source)\r\n\tat io.restassured.internal.ResponseSpecificationImpl.validateResponseIfRequired(ResponseSpecificationImpl.groovy:674)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat org.codehaus.groovy.runtime.callsite.PlainObjectMetaMethodSite.doInvoke(PlainObjectMetaMethodSite.java:43)\r\n\tat org.codehaus.groovy.runtime.callsite.PogoMetaMethodSite$PogoCachedMethodSiteNoUnwrapNoCoerce.invoke(PogoMetaMethodSite.java:193)\r\n\tat org.codehaus.groovy.runtime.callsite.PogoMetaMethodSite.callCurrent(PogoMetaMethodSite.java:61)\r\n\tat org.codehaus.groovy.runtime.callsite.AbstractCallSite.callCurrent(AbstractCallSite.java:185)\r\n\tat io.restassured.internal.ResponseSpecificationImpl.statusCode(ResponseSpecificationImpl.groovy:126)\r\n\tat io.restassured.specification.ResponseSpecification$statusCode$0.callCurrent(Unknown Source)\r\n\tat io.restassured.internal.ResponseSpecificationImpl.statusCode(ResponseSpecificationImpl.groovy:134)\r\n\tat io.restassured.internal.ValidatableResponseOptionsImpl.statusCode(ValidatableResponseOptionsImpl.java:89)\r\n\tat io.restassured.internal.ValidatableResponseImpl.super$2$statusCode(ValidatableResponseImpl.groovy)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat org.codehaus.groovy.reflection.CachedMethod.invoke(CachedMethod.java:107)\r\n\tat groovy.lang.MetaMethod.doMethodInvoke(MetaMethod.java:323)\r\n\tat groovy.lang.MetaClassImpl.invokeMethod(MetaClassImpl.java:1262)\r\n\tat org.codehaus.groovy.runtime.ScriptBytecodeAdapter.invokeMethodOnSuperN(ScriptBytecodeAdapter.java:144)\r\n\tat io.restassured.internal.ValidatableResponseImpl.statusCode(ValidatableResponseImpl.groovy:142)\r\n\tat io.restassured.internal.ValidatableResponseImpl.statusCode(ValidatableResponseImpl.groovy)\r\n\tat stepdefinitions.US27ApiStepdefs.theUserShouldBeDeleteAState(US27ApiStepdefs.java:54)\r\n\tat ✽.The user should be delete a state(file:///C:/Users/user/IdeaProjects/com.gmibank/src/test/resources/features/US27ApiDeleteStates.feature:5)\r\n",
-  "status": "failed"
+  "status": "skipped"
 });
 });
